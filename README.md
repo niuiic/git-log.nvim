6c72fd80e826fc9d0118c2677acae36a28c75994
niuiic <niuiicde@gmail.com>
2025-02-26 19:29:08 +0800

    refactor: *

--- README.md
+++ README.md
@@ -8,31 +8,21 @@ Check git log of the selected code.
 
 Just call `require("git-log").check_log()`.
 
+Avaiable options:
+
+```lua
+require("git-log").check_log({
+	extra_args = {},
+	window_width_ratio = 0.6,
+	window_height_ratio = 0.8,
+	quit_key = "q",
+})
+```
+
 > Remember to save the file before calling this function.
 
 <img src="https://github.com/niuiic/assets/blob/main/git-log.nvim/usage.gif" />
 
 ## Dependencies
 
-- [niuiic/core.nvim](https://github.com/niuiic/core.nvim)
-
-## Config
-
-Default configuration here.
-
-```lua
-{
-	-- args of git log
-	extra_args = {},
-	-- window options
-	win = {
-		border = "rounded",
-		width_ratio = 0.6,
-		height_ratio = 0.8,
-	},
-	keymap = {
-		-- keymap to close the window
-		close = "q",
-	},
-}
-```
+- [niuiic/omega.nvim](https://github.com/niuiic/omega.nvim)
--- lua/git-log/init.lua
+++ lua/git-log/init.lua
@@ -1,76 +1,96 @@
-local static = require("git-log.static")
-local core = require("core")
-local utils = require("git-log.utils")
+local M = {}
 
-local setup = function(new_config)
-	static.config = vim.tbl_deep_extend("force", static.config, new_config or {})
+function M.check_log(options)
+	options = options or {}
+
+	local file_name = vim.api.nvim_buf_get_name(0)
+	if not file_name then
+		vim.notify("buffer is not valid", vim.log.levels.WARN)
+		return
+	end
+
+	local selection_area = require("omega").get_selected_area()
+	local commands = { "git", "log" }
+	commands = vim.list_extend(commands, options.extra_args or {})
+	commands = vim.list_extend(
+		commands,
+		{ "-L", string.format("%s,%s:%s", selection_area.start_lnum, selection_area.end_lnum, file_name) }
+	)
+
+	vim.system(
+		commands,
+		{ cwd = vim.fs.root(0, ".git") },
+		vim.schedule_wrap(function(result)
+			if result.code ~= 0 then
+				vim.notify(result.stderr, vim.log.levels.ERROR)
+				return
+			end
+			M._display_log(
+				result.stdout,
+				options.window_width_ratio or 0.6,
+				options.window_height_ratio or 0.8,
+				options.quit_key or "q"
+			)
+		end)
+	)
 end
 
-local check_log = function()
-	local bufnr = vim.api.nvim_create_buf(false, true)
-	local win_size_opts = core.win.proportional_size(static.config.win.width_ratio, static.config.win.height_ratio)
+function M._display_log(log, width_ratio, height_ratio, quit_key)
+	local lines = vim.split(log, require("omega").get_line_ending(0))
 
-	-- check log
-	local file_name = vim.api.nvim_buf_get_name(0)
-	local line_range = utils.range()
-	local args = { "git", "log" }
-	args = vim.list_extend(args, static.config.extra_args)
-	args = vim.list_extend(args, { "-L", string.format("%s,%s:%s", line_range[1], line_range[2], file_name) })
-	local result = vim.system(args, { cwd = vim.fs.root(0, ".git") }):wait()
-
-	-- format log
-	local log = core.lua.string.split(result.stdout, "\n")
-	local new_log = {}
-	local first_commit = true
-	for i = 1, table.maxn(log), 1 do
-		if string.match(log[i], "^commit") then
-			if first_commit then
-				first_commit = false
-			else
-				table.insert(new_log, utils.separator(win_size_opts.width))
-			end
+	local separate_lnums = {}
+	for lnum, line in ipairs(lines) do
+		if string.match(line, "^commit") then
+			table.insert(separate_lnums, lnum)
 		end
-		table.insert(new_log, log[i])
 	end
-	log = new_log
 
-	-- open float window
-	core.win.open_float(bufnr, {
-		enter = true,
+	local bufnr = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_open_win(bufnr, true, M._get_window_options(width_ratio, height_ratio))
+	vim.api.nvim_set_option_value("filetype", "git", { buf = bufnr })
+	vim.keymap.set("n", quit_key, function()
+		pcall(vim.api.nvim_buf_delete, bufnr, { force = true })
+	end, { buffer = bufnr })
+
+	vim.api.nvim_set_option_value("modifiable", true, { buf = bufnr })
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+	vim.api.nvim_set_option_value("modifiable", false, { buf = bufnr })
+
+	local columns = vim.opt.columns:get()
+	local chars = string.rep("-", columns)
+	local ns_id = vim.api.nvim_create_namespace("git-log")
+
+	for i, lnum in ipairs(separate_lnums) do
+		if i ~= 1 then
+			vim.api.nvim_buf_set_extmark(bufnr, ns_id, lnum - 1, 0, {
+				virt_lines = { { { chars, "CursorLineNr" } } },
+				virt_lines_above = true,
+			})
+		end
+	end
+end
+
+function M._get_window_options(width_ratio, height_ratio)
+	local screen_w = vim.opt.columns:get()
+	local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+	local window_w = screen_w * width_ratio
+	local window_h = screen_h * height_ratio
+	local window_w_int = math.floor(window_w)
+	local window_h_int = math.floor(window_h)
+	local center_x = (screen_w - window_w) / 2
+	local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+
+	return {
 		relative = "editor",
-		width = win_size_opts.width,
-		height = win_size_opts.height,
-		row = win_size_opts.row,
-		col = win_size_opts.col,
-		border = static.config.win.border,
+		width = window_w_int,
+		height = window_h_int,
+		row = center_y,
+		col = center_x,
+		border = "rounded",
 		title = "git log",
 		style = "minimal",
 		title_pos = "center",
-	})
-
-	-- set buffer
-	vim.api.nvim_set_option_value("modifiable", true, {
-		buf = bufnr,
-	})
-	vim.api.nvim_set_option_value("filetype", "git", {
-		buf = bufnr,
-	})
-	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, log)
-	vim.api.nvim_set_option_value("modifiable", false, {
-		buf = bufnr,
-	})
-
-	-- set keymap
-	vim.keymap.set("n", static.config.keymap.close, function()
-		pcall(vim.api.nvim_buf_delete, bufnr, {
-			force = true,
-		})
-	end, {
-		buffer = bufnr,
-	})
+	}
 end
 
-return {
-	setup = setup,
-	check_log = check_log,
-}
+return M
